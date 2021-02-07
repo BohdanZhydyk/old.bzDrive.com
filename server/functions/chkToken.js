a3ee161efd51cdeb1387dfafc7cc48af0b38ff76
Bohdan Zhydyk <bzua83@gmail.com>
2021-02-07 23:13:13 +0100

    edit chkToken function

--- server/functions/chkToken.js
+++ server/functions/chkToken.js
@@ -14,26 +14,22 @@ exports.chkToken = (req, res)=>{
     if (error){ console.log("can't connect to the DB") }
     else{
     
-      client.db(dbName).collection('bzAuth').findOne( {bzToken}, (error, result)=>{
+      client.db(dbName).collection('bzAuth').find({bzToken}).sort({_id:-1}).limit(1).toArray( (error, result)=>{
+        
         if(error){ if(error){ console.log(error) } }
         else{
-          if(result){
-            bzToken = result.bzToken
-            user = result.user
+
+          if(!result[0]){
+            bzToken = generateToken(64)
+            user = {login: "man", role: "guest", lang: false, sex: false, ava: false}
           }
           else{
-            bzToken = generateToken(64)
-            user = {
-              login: "man",
-              role: "guest",
-              lang: false,
-              sex: false,
-              ava: false
-            }
+            bzToken = result[0].bzToken
+            user = result[0].user
           }
+
+          if( !IP ){ IP = "undefined" }
         }
-    
-        if( !IP ){ IP = "undefined" }
     
         client.db(dbName).collection('bzAuth').insertOne( {bzToken, user, from, IP} )
     
